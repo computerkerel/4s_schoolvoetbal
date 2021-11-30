9ef643932fed3fa818fc3762d6482b69d9eb9ae5
Sam <sambrands2001@gmail.com>
2021-11-30 13:54:19 +0100

    Begin gemaakt voor basis lay-out

---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="nl">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Schoolvoetbal</title>
+</head>
+<body>
+	<header>
+		<nav>
+
+		</nav>
+	</header>
+
+	<main>
+
+	</main>
+
+	<footer>
+
+	</footer>
+</body>
+</html>
