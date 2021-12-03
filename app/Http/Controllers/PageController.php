922c95cc2203a2c112fcbb7f5926581dc0ce616d
Sam <sambrands2001@gmail.com>
2021-12-03 14:36:11 +0100

    Wedstrijden uit PageController gehaald
    Omdat we nu een WedstrijdenController hebben

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -18,18 +18,11 @@ class PageController extends Controller
     {
         return view('pages/dashboard');
     }
-
-
+    
     public function teams()
     {
         $teams = Team::all();
         return view('pages/teams')->with(['teams' => $teams]);
-    }
-
-    public function wedstrijden()
-    {
-        $wedstrijden = Wedstrijd::all();
-        return view('pages/wedstrijden')->with(['wedstrijden' => $wedstrijden]);
     }
 
 }
