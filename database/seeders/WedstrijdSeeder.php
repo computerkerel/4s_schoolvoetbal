b0c78b68071d2b631b1ac2d7fe272b0f326abb8c
Sam <sambrands2001@gmail.com>
2021-12-06 11:33:59 +0100

    Datum kolom toegevoegd in seeder
    In de WedstrijdenSeeder wordt nu ook een datum ingevoerd

--- database/seeders/WedstrijdSeeder.php
+++ database/seeders/WedstrijdSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 
-
 use App\Models\Wedstrijd;
 use Illuminate\Database\Seeder;
 
@@ -12,32 +11,33 @@ class WedstrijdSeeder extends Seeder
     /**
      * Run the database seeds.
      *
-
      * @return void
      */
     public function run()
     {
         $wedstrijd = new Wedstrijd();
-        $wedstrijd-> title = "4s schoolvoetbal vs fc Breda";
-        $wedstrijd-> team1 =  1;
-        $wedstrijd-> team2 =  2;
-        $wedstrijd-> score_team1 =  1;
-        $wedstrijd-> score_team2 =  3;
-        $wedstrijd-> locatie =  'nac station';
-        $wedstrijd-> scheidsrechter = 'Danny Makkelie';
-        $wedstrijd-> status = "Fc Breda heeft gewonen";
-        $wedstrijd -> save();
+        $wedstrijd->title = "4S Schoolvoetbal vs FC Breda";
+        $wedstrijd->team1 = 1;
+        $wedstrijd->team2 = 2;
+        $wedstrijd->score_team1 = 1;
+        $wedstrijd->score_team2 = 3;
+        $wedstrijd->locatie = 'NAC Stadion';
+        $wedstrijd->datum = '2021-12-20 18:00:00';
+        $wedstrijd->scheidsrechter = 'Danny Makkelie';
+        $wedstrijd->status = "FC Breda heeft gewonen";
+        $wedstrijd->save();
 
         $wedstrijd = new Wedstrijd();
-        $wedstrijd-> title = "4s schoolvoetbal vs fc Breda";
-        $wedstrijd-> team1 =  1;
-        $wedstrijd-> team2 =  2;
-        $wedstrijd-> score_team1 =  4;
-        $wedstrijd-> score_team2 =  2;
-        $wedstrijd-> locatie =  'dorst station';
-        $wedstrijd-> scheidsrechter = ' Bas Nijhuis';
-        $wedstrijd-> status = "4s schoolvoetbal heeft gewonen";
-        $wedstrijd -> save();
+        $wedstrijd->title = "4S Schoolvoetbal vs FC Breda";
+        $wedstrijd->team1 = 1;
+        $wedstrijd->team2 = 2;
+        $wedstrijd->score_team1 = 4;
+        $wedstrijd->score_team2 = 2;
+        $wedstrijd->locatie = 'Dorst Stadion';
+        $wedstrijd->datum = '2022-1-19 18:00:00';
+        $wedstrijd->scheidsrechter = ' Bas Nijhuis';
+        $wedstrijd->status = "4S Schoolvoetbal heeft gewonen";
+        $wedstrijd->save();
 
 
     }
