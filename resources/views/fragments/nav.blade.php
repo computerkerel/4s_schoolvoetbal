b5d8b14b3832212917e9b70cad8c429bbe919d2c
Sam <sambrands2001@gmail.com>
2021-12-06 10:34:42 +0100

    Navbar items active class
    De pagina waar de gebruiker nu op is krijgt in de navbar de class 'active'

--- resources/views/fragments/nav.blade.php
+++ resources/views/fragments/nav.blade.php
@@ -1,4 +1,3 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
 <nav class="navbar navbar-expand-lg navbar-light bg-light px-5">
 	<div class="container-fluid">
 		<a class="navbar-brand" href="#">Navbar</a>
@@ -7,9 +6,9 @@
 		</button>
 		<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
 			<div class="navbar-nav">
-				<a class="nav-link @if(request()->is('/')) active @endif" aria-current="page" href="#">Home</a>
-				<a class="nav-link @if(request()->is('/wedstrijden')) active @endif" href="#">Wedstrijden</a>
-				<a class="nav-link @if(request()->is('/teams')) active @endif" href="#">Teams</a>
+				<a class="nav-link @if(request()->is('/')) active @endif" aria-current="page" href="{{route('home')}}">Home</a>
+				<a class="nav-link @if(request()->is('admin/wedstrijden')) active @endif" href="{{route('wedstrijden.index')}}">Wedstrijden</a>
+				<a class="nav-link @if(request()->is('admin/teams')) active @endif" href="#">Teams</a>
 			</div>
 		</div>
 	</div>
