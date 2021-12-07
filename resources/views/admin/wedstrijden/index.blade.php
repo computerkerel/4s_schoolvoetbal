8ab93b39699da5460a89ff1b4a67ff225c43e6d7
Sam <sambrands2001@gmail.com>
2021-12-07 12:00:37 +0100

    Wedstrijden index basis

---
+++ resources/views/admin/wedstrijden/index.blade.php
@@ -0,0 +1,33 @@
+@extends('pages.base')
+
+@section('content')
+	<div class="container">
+		<table class="table">
+			<thead>
+				<tr>
+					<th>Team 1</th>
+					<th>Team 2</th>
+					<th>Uitslag</th>
+					<th>Locatie</th>
+					<th>Datum</th>
+					<th>Scheidsrechter</th>
+					<th>Status</th>
+				</tr>
+			</thead>
+			<tbody>
+				@foreach($wedstrijden as $wedstrijd)
+					<tr>
+						<td>{{$wedstrijd->team1_id}}</td>
+						<td>{{$wedstrijd->team2_id}}</td>
+						<td>{{$wedstrijd->score_team1}}-{{$wedstrijd->score_team2}}</td>
+						<td>{{$wedstrijd->locatie}}</td>
+						<td>{{$wedstrijd->datum}}</td>
+						<td>{{$wedstrijd->scheidsrechter}}</td>
+						<td>{{$wedstrijd->status}}</td>
+					</tr>
+				@endforeach
+			</tbody>
+		</table>
+	</div>
+@endsection
+
