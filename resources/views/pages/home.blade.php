5641cfdb1e4f0573f86243dc969b37cb9e1dd5f0
Ibrahim <72141548+ibrahimhillowle@users.noreply.github.com>
2021-12-09 14:06:13 +0100

    Wedstrijden op de home pagina tonen

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -5,7 +5,18 @@
 		<div class="row g-5 py-5 justify-content-around h-50">
 			<div class="col-md-6 h-100">
 				<div class="border px-3 bg-light">
-					<h2>Volgende wedstrijden</h2>
+					<h1  style="text-align: center">Volgende wedstrijden</h1>
+                        @foreach($wedstrijden as $wedstrijd)
+                        <h2 style="text-align: center">{{$wedstrijd->team1->teamnaam}} Vs {{$wedstrijd->team2->teamnaam}} </h2>
+                    <p  style="text-align: center">Op  {{$wedstrijd->locatie}} om {{$wedstrijd->datum}} </p>
+
+
+
+
+
+
+                        @endforeach
+
 				</div>
 			</div>
 
