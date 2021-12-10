ca00bbd7de80aacc67dfeec5638b40f9f4f486b5
Sam <sambrands2001@gmail.com>
2021-12-10 11:17:33 +0100

    Team aanmaken pagina

---
+++ resources/views/admin/teams/create.blade.php
@@ -0,0 +1,35 @@
+@extends('pages.base')
+
+@section('content')
+	<div class="container">
+		<h1>Nieuw team</h1>
+
+		@if ($errors->any())
+			<div class="alert alert-danger">
+				<ul>
+					@foreach ($errors->all() as $error)
+						<li>{{ $error }}</li>
+					@endforeach
+				</ul>
+			</div>
+		@endif
+
+		<form action="{{route('teams.store')}}" method="POST" class="w-50 mx-auto">
+			@csrf
+			<div class="mb-3">
+				<label for="teamnaam" class="form-label">Teamnaam</label>
+				<input type="text" class="form-control" id="teamnaam" name="teamnaam" value="{{old('teamnaam')}}">
+			</div>
+
+			<div class="mb-3">
+				<label for="ingeschreven_door" class="form-label">Ingeschreven door</label>
+				<input type="text" class="form-control" name="ingeschreven_door" id="disabledTextInput" value="{{Auth::user()->name}}" readonly>
+			</div>
+
+			<div class="mb-3">
+				<input type="submit" class="btn btn-primary" value="Opslaan">
+			</div>
+
+		</form>
+	</div>
+@endsection
