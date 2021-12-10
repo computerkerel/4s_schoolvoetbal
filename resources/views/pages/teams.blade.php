b2584a63fb3bb5f8bee5b2521e9210ca06239084
Sam <sambrands2001@gmail.com>
2021-12-10 10:39:21 +0100

    Teams code format

--- resources/views/pages/teams.blade.php
+++ resources/views/pages/teams.blade.php
@@ -1,29 +1,30 @@
 @extends('pages.base')
 
 @section('content')
+	<div class="container">
 
+		<h1>Teams</h1>
 
+		@include('fragments.flash-message')
 
-
-   <div class="container">
-       <table class="table table">
-           <thead>
-           <tr>
-               <th scope="col">Name</th>
-               <th scope="col">Team</th>
-           </tr>
-           </thead>
-           <tbody>
-           @foreach($teams as $team)
-           <tr>
-               <td>{{$team->ingeschreven_door}}</td>
-               <td>{{$team->teamnaam}}</td>
-           </tr>
-           @endforeach
-           </tbody>
-       </table>
-       <img src="https://source.unsplash.com/user/erondu/1300x400">
-   </div>
+		<table class="table">
+			<thead>
+				<tr>
+					<th>Name</th>
+					<th>Team</th>
+				</tr>
+			</thead>
+			<tbody>
+				@foreach($teams as $team)
+					<tr>
+						<td>{{$team->ingeschreven_door}}</td>
+						<td>{{$team->teamnaam}}</td>
+					</tr>
+				@endforeach
+			</tbody>
+		</table>
+		<img src="https://source.unsplash.com/user/erondu/1300x400">
+	</div>
 
 @endsection
 
