7e71076551bc2720e70c3ca2fda372018633972b
Sam <sambrands2001@gmail.com>
2021-12-12 12:48:49 +0100

    Modal toegevoegd voor verwijderen van team
    Gebruiker krijgt een popup te zien en kan het verwijderen bevestigen of annuleren

--- resources/views/admin/teams/show.blade.php
+++ resources/views/admin/teams/show.blade.php
@@ -13,7 +13,42 @@
 		<form action="{{route('teams.destroy', $team->id)}}" method="POST">
 			@csrf
 			@method('DELETE')
-			<input type="submit" class="btn btn-danger" value="Verwijderen">
+			<input type="button" class="btn btn-danger" id="delete-team" value="Verwijderen" data-bs-toggle="modal"
+			       data-bs-target="#deleteTeamModal">
+
+			<!-- Modal -->
+			<div class="modal fade" id="deleteTeamModal" tabindex="-1" aria-labelledby="deleteTeamModal"
+			     aria-hidden="true">
+				<div class="modal-dialog">
+					<div class="modal-content">
+						<div class="modal-header">
+							<h5 class="modal-title" id="deleteTeamModalLabel">Team verwijderen?</h5>
+							<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+						</div>
+						<div class="modal-body">
+							<p>Als je dit team verwijderd worden ook alle wedstrijden van dit team verwijderd.</p>
+							<p><strong>Weet je het zeker?</strong></p>
+						</div>
+						<div class="modal-footer">
+							<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annuleren</button>
+							<button type="submit" class="btn btn-danger">Verwijderen</button>
+						</div>
+					</div>
+				</div>
+			</div>
+
+
 		</form>
 	</div>
+
+	<script>
+        var modal = document.getElementById('deleteTeamModal');
+        var trigger = document.getElementById('delete-team');
+
+        modal.addEventListener('shown.bs.modal', function () {
+            trigger.focus()
+            event.preventDefault();
+        });
+	</script>
+
 @endsection
