1e7292edf9ddff54491c1fca57d83824f7716f10
Sam <sambrands2001@gmail.com>
2021-12-17 13:57:38 +0100

    Return type comment
    Om de kringels onder de code te voorkomen

--- app/Http/Controllers/MatchController.php
+++ app/Http/Controllers/MatchController.php
@@ -82,7 +82,7 @@ class MatchController extends Controller
      * Show the form for editing the specified resource.
      *
      * @param int $id
-     * @return \Illuminate\Http\Response
+//     * @return \Illuminate\Http\Response
      */
     public function edit($id)
     {
