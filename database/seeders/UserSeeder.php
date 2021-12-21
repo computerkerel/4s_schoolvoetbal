6cd881d640822ba0f6bdb195f93b67a424ea9f0d
Sam Brands <sambrands2001@gmail.com>
2021-12-21 10:44:57 +0100

    Rolen toegevoegd in UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -18,12 +18,14 @@ class UserSeeder extends Seeder
         $user->name = "test";
         $user->email = "example@example.com";
         $user->password = '$2y$10$f8EeZ0nWtwFaECWuwhIYJuCB7I.JtMuFZAr34VXvYMU83x5C/Q10q';
+        $user->role = 3;
         $user->save();
 
         $user = new User();
         $user->name = "Wicher";
         $user->email = "wicher@wicher.nl";
         $user->password = '$2y$10$f8EeZ0nWtwFaECWuwhIYJuCB7I.JtMuFZAr34VXvYMU83x5C/Q10q';
+        $user->role = 2;
         $user->save();
     }
 }
