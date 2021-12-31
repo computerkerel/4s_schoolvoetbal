0ee66134ae73491469b738f4c4bb82b83a9cb681
Sam Brands <sambrands2001@gmail.com>
2021-12-31 17:11:57 +0100

    Code format

--- resources/views/admin/veld/show.blade.php
+++ resources/views/admin/veld/show.blade.php
@@ -1,17 +1,17 @@
 @extends('pages.base')
 
 @section('content')
-    <div class="container">
-        <h1>Velden Detailpagina</h1>
-        <hr>
+	<div class="container">
+		<h1>Velden Detailpagina</h1>
+		<hr>
 
-            <p>{{$fields->naam}}</p>
-        <a href="{{route('fields.edit', $fields->id)}}" class="btn btn-info">Aanpassen</a>
+		<p>{{$fields->naam}}</p>
+		<a href="{{route('fields.edit', $fields->id)}}" class="btn btn-info">Aanpassen</a>
 
-        <form action="{{route('fields.destroy', $fields->id)}}" method="POST">
-            @csrf
-            @method('DELETE')
-            <input type="submit" class="btn btn-danger" value="Verwijderen">
-        </form>
-    </div>
+		<form action="{{route('fields.destroy', $fields->id)}}" method="POST">
+			@csrf
+			@method('DELETE')
+			<input type="submit" class="btn btn-danger" value="Verwijderen">
+		</form>
+	</div>
 @endsection
