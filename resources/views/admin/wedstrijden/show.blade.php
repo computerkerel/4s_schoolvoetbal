843ae0eff16d78a32d5b93df2d88fa2199b66b36
Sam Brands <sambrands2001@gmail.com>
2021-12-31 17:27:01 +0100

    Veldnaam op wedstrijd detailpagina

--- resources/views/admin/wedstrijden/show.blade.php
+++ resources/views/admin/wedstrijden/show.blade.php
@@ -8,11 +8,9 @@
 		<p><strong>Team 1:</strong> {{$wedstrijd->team1->teamnaam}}</p>
 		<p><strong>Team 2:</strong> {{$wedstrijd->team2->teamnaam}}</p>
 		<p><strong>Datum:</strong> {{$wedstrijd->datum}}</p>
-		<p><strong>Locatie:</strong> {{$wedstrijd->locatie}}</p>
+		<p><strong>Locatie:</strong> {{$wedstrijd->field->naam}}</p>
 		<p><strong>Scheidsrechter:</strong> {{$wedstrijd->scheidsrechter->name}}</p>
 		<p><strong>Status:</strong> {{$wedstrijd->status}}</p>
-
-        <p><strong>Status:</strong> {{$wedstrijd->fields}}</p>
 
 		<a href="{{route('wedstrijden.edit', $wedstrijd->id)}}" class="btn btn-info">Aanpassen</a>
 
