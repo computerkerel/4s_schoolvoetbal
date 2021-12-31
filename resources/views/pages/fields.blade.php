20024335875a8ed1398089529d887424fc7d5796
Sam Brands <sambrands2001@gmail.com>
2021-12-31 17:03:16 +0100

    Container toegevoegd aan velden

--- resources/views/pages/fields.blade.php
+++ resources/views/pages/fields.blade.php
@@ -1,27 +1,26 @@
 @extends('pages.base')
 
 @section('content')
-
-
-
-
-    <table class="table">
-        <thead>
-        <tr>
-            <th>Velden</th>
-
-        </tr>
-        </thead>
-        <tbody>
-        @foreach($fields as $field)
-            <tr>
-                <td>
-                    <a href="{{route('fields.show', $field->id)}}">{{$field->naam}}</a></td></td>
-            </tr>
-        @endforeach
-        </tbody>
-    </table>
-    @auth()
-        <a href="{{route('fields.create')}}" class="btn btn-primary"><i class="bi bi-plus-square"></i> Nieuwe Veld</a>
-    @endauth
+	<div class="container">
+		<table class="table">
+			<thead>
+				<tr>
+					<th>Velden</th>
+				</tr>
+			</thead>
+			<tbody>
+				@foreach($fields as $field)
+					<tr>
+						<td>
+							<a href="{{route('fields.show', $field->id)}}">{{$field->naam}}</a>
+						</td>
+					</tr>
+				@endforeach
+			</tbody>
+		</table>
+		@auth()
+			<a href="{{route('fields.create')}}" class="btn btn-primary"><i class="bi bi-plus-square"></i> Nieuwe Veld
+			</a>
+		@endauth
+	</div>
 @endsection
