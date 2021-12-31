d2a26bf7bd8022ae1f55b152389e1901dde91e62
Sam Brands <sambrands2001@gmail.com>
2021-12-31 17:11:03 +0100

    Styling voor aanmaakformulier voor velden

--- resources/views/admin/veld/create.blade.php
+++ resources/views/admin/veld/create.blade.php
@@ -6,16 +6,19 @@
 
 		@include('fragments.flash-message')
 
-		<form action="{{route('fields.store')}}" method="post">
+		<form action="{{route('fields.store')}}" method="POST" class="w-50 mx-auto">
 			@csrf
-			<div class="form-group">
-				<input type="text" name="naam">
+			<div class="mb-3">
+				<label for="naam" class="form-label">Veldnaam</label>
+				<input type="text" class="form-control" name="naam" value="{{old('naam')}}">
 			</div>
+
 			<div class="mb-3">
 				<label for="aangemaakt_door" class="form-label">Ingeschreven door</label>
 				<input type="text" class="form-control" name="aangemaakt_door" id="disabledTextInput"
 				       value="{{Auth::user()->name}}" readonly>
 			</div>
+
 			<div class="mb-3">
 				<input type="submit" class="btn btn-primary" value="Opslaan">
 			</div>
