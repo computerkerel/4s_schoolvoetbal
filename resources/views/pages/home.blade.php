8d7eee53bfb42c5fc9ce8ab3c18c67345575c5ec
Sam Brands <sambrands2001@gmail.com>
2022-01-07 15:03:29 +0100

    Code format voor homepagina
    Ook een hr toegevoegd en wat classes en padding aangepast

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,26 +4,31 @@
 	<div class="container-lg mg-auto">
 		<div class="row g-5 py-5 justify-content-around h-50">
 			<div class="col-md-6 h-100">
-				<div class="border px-3 bg-light">
-					<h1  style="text-align: center">Volgende wedstrijden</h1>
-                        @foreach($wedstrijden as $wedstrijd)
-                        <h2 style="text-align: center">{{$wedstrijd->team1->teamnaam}} Vs {{$wedstrijd->team2->teamnaam}} </h2>
-                    <p  style="text-align: center">Op  {{$wedstrijd->locatie}} om {{$wedstrijd->datum}} </p>
-
-
-
-
-
-
-                        @endforeach
-
+				<div class="border px-3 bg-light p-2">
+					<h1 class="text-center">Volgende wedstrijden</h1>
+					<hr>
+					@foreach($wedstrijden as $wedstrijd)
+						<h2 class="text-center">
+							{{$wedstrijd->team1->teamnaam}} vs {{$wedstrijd->team2->teamnaam}}
+						</h2>
+						<p class="text-center">
+							Op {{$wedstrijd->field->naam}} om {{$wedstrijd->datum}}
+						</p>
+					@endforeach
 				</div>
 			</div>
 
 			<div class="col-md-6 h-100">
 				<div class="border px-3 bg-light">
 					<h2>Welkom</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus adipisci aliquam aut dicta distinctio ducimus, eius enim eos error ex explicabo facere facilis id, illo labore libero maiores nesciunt nisi non nulla numquam quo repellat, sequi similique voluptatem. Consequatur illum ipsum minima non quidem recusandae tenetur. Aliquid consequatur cum, esse est excepturi incidunt ipsam, libero minus numquam officiis rem vel vero? Consequatur dolore dolorum id omnis quaerat sint sunt tenetur. A amet animi asperiores assumenda atque delectus dicta dolor, dolores doloribus error et impedit ipsam, itaque laborum mollitia nemo neque officia pariatur placeat porro quos sit sunt ut? Beatae, non?</p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus adipisci aliquam aut dicta
+						distinctio ducimus, eius enim eos error ex explicabo facere facilis id, illo labore libero
+						maiores nesciunt nisi non nulla numquam quo repellat, sequi similique voluptatem. Consequatur
+						illum ipsum minima non quidem recusandae tenetur. Aliquid consequatur cum, esse est excepturi
+						incidunt ipsam, libero minus numquam officiis rem vel vero? Consequatur dolore dolorum id omnis
+						quaerat sint sunt tenetur. A amet animi asperiores assumenda atque delectus dicta dolor, dolores
+						doloribus error et impedit ipsam, itaque laborum mollitia nemo neque officia pariatur placeat
+						porro quos sit sunt ut? Beatae, non?</p>
 				</div>
 			</div>
 		</div>
