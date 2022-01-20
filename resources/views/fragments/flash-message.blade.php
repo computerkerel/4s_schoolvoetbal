a6206168c13209cc3bc129f6976ff66ac52fe4d2
Sam <sambrands2001@gmail.com>
2022-01-20 13:28:44 +0100

    Flash message update voor validatie errors
    Validatie errors worden nu weergegeven in de flash-message fragment

--- resources/views/fragments/flash-message.blade.php
+++ resources/views/fragments/flash-message.blade.php
@@ -11,6 +11,16 @@
 	</div>
 @endif
 
+@if ($errors->any())
+	<div class="alert alert-danger">
+		<ul>
+			@foreach ($errors->all() as $error)
+				<li>{{ $error }}</li>
+			@endforeach
+		</ul>
+	</div>
+@endif
+
 
 @if ($message = Session::get('warning'))
 	<div class="alert alert-warning alert-block">
