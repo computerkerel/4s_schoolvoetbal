074c1019c690ad0f5ff6bed441493f354465194a
Sam <sambrands2001@gmail.com>
2022-01-27 13:47:18 +0100

    Wedstrijden show pagina fix

--- resources/views/admin/wedstrijden/show.blade.php
+++ resources/views/admin/wedstrijden/show.blade.php
@@ -8,9 +8,9 @@
 		<p><strong>Team 1:</strong> {{$wedstrijd->team1->teamnaam}}</p>
 		<p><strong>Team 2:</strong> {{$wedstrijd->team2->teamnaam}}</p>
 		<p><strong>Datum:</strong> {{date('d-m-Y H:i', strtotime($wedstrijd->datum))}}</p>
-		<p><strong>Locatie:</strong> {{$wedstrijd->field->naam}}</p>
-		<p><strong>Scheidsrechter:</strong> {{$wedstrijd->scheidsrechter->name}}</p>
-		<p><strong>Status:</strong> {{$wedstrijd->status}}</p>
+		<p><strong>Locatie:</strong> {{$wedstrijd->field->naam ?? ''}}</p>
+		<p><strong>Scheidsrechter:</strong> {{$wedstrijd->scheidsrechter->name ?? ''}}</p>
+		<p><strong>Status:</strong> {{$wedstrijd->status ?? ''}}</p>
 
 		<a href="{{route('wedstrijden.edit', $wedstrijd->id)}}" class="btn btn-info">Aanpassen</a>
 
