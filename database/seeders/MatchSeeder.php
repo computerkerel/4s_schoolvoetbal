cdcd919797855bed49ff6a018b81265f02ad8164
Sam <sambrands2001@gmail.com>
2022-01-27 13:41:46 +0100

    Wedstrijd seeder fix

--- database/seeders/MatchSeeder.php
+++ database/seeders/MatchSeeder.php
@@ -24,17 +24,19 @@ class MatchSeeder extends Seeder
         $wedstrijd->field_id = 1;
         $wedstrijd->datum = '2021-12-20 18:00:00';
         $wedstrijd->scheidsrechter_id = 1;
-        $wedstrijd->status = "FC Breda heeft gewonen";
+        $wedstrijd->status = "FC Tilburg heeft gewonen";
         $wedstrijd->save();
 
         $wedstrijd = new Match();
-        $wedstrijd->title = "4S Schoolvoetbal vs FC Breda";
+        $wedstrijd->title = "FC Tilburg vs FC Breda";
         $wedstrijd->team1_id = 1;
         $wedstrijd->team2_id = 2;
+        $wedstrijd->score_team1 = 2;
+        $wedstrijd->score_team2 = 4;
         $wedstrijd->field_id = 2;
         $wedstrijd->datum = '2022-1-19 18:00:00';
         $wedstrijd->scheidsrechter_id = 2;
-        $wedstrijd->status = "4S Schoolvoetbal heeft gewonen";
+        $wedstrijd->status = "FC Tilburg heeft gewonen";
         $wedstrijd->save();
 
         $wedstrijd = new Match();
